17470d21855d610885a771ea6ea6018f22bf3d5c
Carlos Adriano Miranda <cadriano.miranda@gmail.com>
2025-04-23 21:01:23 -0300

    adiciona suporte ao Apollo Client e configurações do GraphQL no Vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,11 @@
 // main.js
-import { createApp } from "vue";
+import { createApp, h } from "vue";
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client/core";
+import { createApolloProvider } from "@vue/apollo-option";
 import App from "./App.vue";
 import router from "./router";
 import "./style.css";
@@ -8,7 +14,26 @@ import "./style.css";
 // npm install @fortawesome/fontawesome-free
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const app = createApp(App);
+const httpLink = createHttpLink({
+  uri: "http://localhost:3000/graphql",
+});
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+});
+
+// Criação da aplicação Vue com Apollo
+const app = createApp({
+  render: () => h(App),
+});
+
+// Uso do provider Apollo
+app.use(apolloProvider);
 
 // Use router
 app.use(router);
