ae123232322db67f1e575950268d7cbc2585ed05
Carlos Adriano Miranda <cadriano.miranda@gmail.com>
2025-04-28 15:03:30 -0300

    feat(config): adiciona arquivos de configuração TypeScript e Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import * as path from "path";
 import Components from "unplugin-vue-components/vite";
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
     vue(),
     Components({
